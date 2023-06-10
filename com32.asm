873bf3235ec63ddb76d5213de73424518e7f655f
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-06-10 10:58:03 +0100

    Add files via upload

--- com32.asm
+++ com32.asm
@@ -6,10 +6,18 @@ mov ecx,60000
 mov ax,0
 mov ds,ax
 mov al,1
+mov ah,1
 fors:
 	ds
 	mov [edi],al
 	inc edi
+	mov al,ah
+	mov esi,edi
+	and esi,15
+	cmp esi,8
+	jnz for1
+	mov al,9
+for1:
 	dec ecx
 	cmp ecx,0
 	jnz fors
